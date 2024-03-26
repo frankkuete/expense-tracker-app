f25d50c4754129aca7c044b0423424f89e13f021
loic.yondjeu <loic.yondjeu@siemens.com>
2024-03-26 23:17:37 +0100

    feat: add mongo db connection

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Api, Resource
 from flasgger import Swagger
+from pymongo import MongoClient
 
 app = Flask(__name__)
 api = Api(app)
 swagger = Swagger(app)
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client.get_database("accounts")
+accounts = db.get_collection("accounts")
 
 class Account(Resource):
     def get(self):
@@ -17,6 +22,29 @@ class Account(Resource):
           200:
             description: account returned
         """
+        return { 'id': 123 }
+    def post(self):
+        """
+        creates an account
+        ----
+        tags:
+          - accounts
+        parameters:
+          - in: body
+            name: account
+            schema:
+              type: object
+              properties:
+                name:
+                  type: string
+                balance:
+                  type: number
+        responses:
+          201:
+            description: account created
+        """
+        data = request.get_json()
+        accounts.insert_one(data)
         return { 'id': 123 }
 
 api.add_resource(Account, '/accounts')
