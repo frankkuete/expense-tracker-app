c209cd1f26a044f3ec3af3de41ac6d731af5dbde
loic.yondjeu <loic.yondjeu@siemens.com>
2024-03-27 20:59:56 +0100

    feat: containerize backend

--- backend/app.py
+++ backend/app.py
@@ -62,4 +62,4 @@ class Account(Resource):
 api.add_resource(Account, '/accounts')
 
 if __name__ == '__main__':
-    app.run(port=5001, debug=True)
+    app.run(port=8080, host='0.0.0.0', debug=True)
